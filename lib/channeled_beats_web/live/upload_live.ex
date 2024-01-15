bb9536cc34b9e060ba3f4d6ce239b5dd1b2e1e54
Andreas Stocker <channeledbeats@proton.me>
2024-01-15 14:22:10 +0100

    upload_live: Album collapsable works

--- lib/channeled_beats_web/live/upload_live.ex
+++ lib/channeled_beats_web/live/upload_live.ex
@@ -1,5 +1,65 @@
 defmodule ChanneledBeatsWeb.UploadLive do
   use ChanneledBeatsWeb, :live_view
 
-  
+  def mount(_params, _session, socket) do
+    {:ok, socket |> assign(:selected_collapsable, "fl-studio-tutorial")}
+  end
+
+  def handle_event("select-collapsable", %{"name" => name}, socket) do
+    # Deselect a collapsable if it was already selected 
+    name =
+      if name == socket.assigns.selected_collapsable do
+        ""
+      else
+        name
+      end
+
+    {:noreply, socket |> assign(:selected_collapsable, name)}
+  end
+
+  def collapsable(assigns) do
+    icon_name =
+      if assigns.done do
+        "hero-check-circle"
+      else
+        "hero-exclamation-circle"
+      end
+
+    icon_color =
+      if assigns.done do
+        "text-brand"
+      else
+        "text-amber-500"
+      end
+
+    selected = assigns.name == assigns.selected_collapsable
+
+    title_bg_color =
+      if selected do
+        "bg-zinc-200"
+      else
+        ""
+      end
+
+    ~H"""
+    <div>
+      <button
+        type="button"
+        class={["block w-full py-6 px-4 text-xl font-bold flex", title_bg_color]}
+        phx-click="select-collapsable"
+        phx-value-name={@name}
+      >
+        <div class="grow"><%= @label %></div>
+        <.icon
+          name={icon_name}
+          class={[
+            "h-8 w-8",
+            icon_color
+          ]}
+        />
+      </button>
+      <div class={!selected && "hidden"}><%= render_slot(@inner_block) %></div>
+    </div>
+    """
+  end
 end
