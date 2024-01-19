932473aed22bb9edf1d893063c05392ca0718dba
Andreas Stocker <channeledbeats@proton.me>
2024-01-19 18:16:24 +0100

    add_return_to: fix match not handling empty case

--- lib/channeled_beats_web/add_return_to.ex
+++ lib/channeled_beats_web/add_return_to.ex
@@ -2,8 +2,9 @@ defmodule ChanneledBeatsWeb.AddReturnTo do
   def empty, do: [true, fn url -> url end]
 
   def init_add_return_to(%{"return_to" => ""}), do: empty()
-  def init_add_return_to(%{"return_to" => nil}), do: empty()
 
   def init_add_return_to(%{"return_to" => return_to}),
     do: [false, fn url -> url <> "?" <> URI.encode_query(%{return_to: return_to}) end]
+
+  def init_add_return_to(_), do: empty()
 end
